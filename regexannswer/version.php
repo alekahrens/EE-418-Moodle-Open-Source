15cd309dbb450d183afa16025266e4a1999f855e
Adelaide Oakman <86484895+AdLOak@users.noreply.github.com>
2022-12-08 00:03:22 -0500

    corrects refactoring errors

--- regexannswer/version.php
+++ regexannswer/version.php
@@ -25,9 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'qtype_shortanswer';
+$plugin->component = 'qtype_regexanswer';
 $plugin->version   = 2022112800;
 
 $plugin->requires  = 2022111800;
 
 $plugin->maturity  = MATURITY_STABLE;
+
