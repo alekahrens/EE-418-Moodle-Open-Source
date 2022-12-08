7bc06e9d14202354ade2e21d361faa194e7c2c1e
Adelaide Oakman <86484895+AdLOak@users.noreply.github.com>
2022-12-07 23:44:22 -0500

    Updates db file neglected in refractoring

--- regexannswer/db/upgrade.php
+++ regexannswer/db/upgrade.php
@@ -18,7 +18,7 @@
  * Short-answer question type upgrade code.
  *
  * @package    qtype
- * @subpackage shortanswer
+ * @subpackage regexanswer
  * @copyright  2011 The Open University
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -29,7 +29,7 @@ defined('MOODLE_INTERNAL') || die();
  * Upgrade code for the essay question type.
  * @param int $oldversion the version we are upgrading from.
  */
-function xmldb_qtype_shortanswer_upgrade($oldversion) {
+function xmldb_qtype_regexanswer_upgrade($oldversion) {
     global $CFG;
 
     // Automatically generated Moodle v3.9.0 release upgrade line.
